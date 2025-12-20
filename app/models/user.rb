82b6e0cd02aa379cae1b85729f677ffd2987b46c
Maxim Alexander Lamas <madmaxl@Maxims-MacBook-Air.local>
2025-12-19 22:08:12 -0600

    feat: email verification and fix on task assignations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :owned_projects, class_name: 'Project', dependent: :destroy
   has_many :project_members, dependent: :destroy
